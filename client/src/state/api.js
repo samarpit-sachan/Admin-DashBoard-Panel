d7fd17fa637bf4148d5fd061e043438fc3e58c68
samarpit-sachan <samarpitsachan091@gmail.com>
2023-07-10 15:05:40 +0530

    overview,daily,monthly,breakdown,admin,performance pages implemented

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
     reducerPath: "adminApi",
-    tagTypes: ["User","Products","Customers","Transactions"],
+    tagTypes: ["User","Products","Customers","Transactions","Geography","Sales","Admins","Performance","Dashboard"],
     endpoints: (build) => ({
         getUser: build.query({
             query: (id) => `general/user/${id}`,
@@ -29,6 +29,22 @@ export const api = createApi({
             query: () => "client/geography",
             providesTags: ["Geography"],
           }),
+          getSales: build.query({
+            query: () => "sales/sales",
+            providesTags: ["Sales"],
+          }),
+          getAdmins: build.query({
+            query: () => "management/admins",
+            providesTags: ["Admins"],
+          }),
+          getUserPerformance: build.query({
+            query: (id) => `management/performance/${id}`,
+            providesTags: ["Performance"],
+          }),
+          getDashboard: build.query({
+            query: () => "general/dashboard",
+            providesTags: ["Dashboard"],
+          }),
     })
 })
 
@@ -37,5 +53,9 @@ export const {
     useGetProductsQuery,
     useGetCustomersQuery,
     useGetTransactionsQuery,
-    useGetGeographyQuery
+    useGetGeographyQuery,
+    useGetSalesQuery,
+    useGetAdminsQuery,
+    useGetUserPerformanceQuery,
+    useGetDashboardQuery
 } = api
--- server/index.js
+++ server/index.js
@@ -15,7 +15,9 @@ import User from "./models/User.js"
 import Product from './models/Product.js'
 import ProductStat from './models/ProductStat.js'
 import Transaction from './models/Transaction.js'
-import {dataUser,dataProduct,dataProductStat,dataTransaction} from "./data/index.js"
+import OverallStat from './models/OverallStat.js'
+import AffiliateStat from './models/AffiliateStat.js'
+import {dataUser,dataProduct,dataProductStat,dataTransaction,dataOverallStat, dataAffiliateStat} from "./data/index.js"
 
 
 
@@ -49,5 +51,7 @@ mongoose.connect(process.env.MONGO_URL, {
     // Product.insertMany(dataProduct)
     // ProductStat.insertMany(dataProductStat)
     // Transaction.insertMany(dataTransaction)
+    // OverallStat.insertMany(dataOverallStat)
+    // AffiliateStat.insertMany(dataAffiliateStat)
     
 }).catch((error) => console.log(`${error} did not connect`))
